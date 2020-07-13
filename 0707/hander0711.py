38c6287a0317b3637273788832038ebe2367a4d4
joselyn <1326549593@qq.com>
2020-07-13 18:52:52 +0800

    0713

--- 0707/hander0711.py
+++ 0707/hander0711.py
@@ -65,6 +65,13 @@ class Hander_index():
         plt.title(u'{}'.format(self.info_name))
         plt.savefig('{}'.format(self.info_name))
 
+    def __get_near_index(self,value,left,right):
+        init  = self.info[left]-value # 若果>0, 则下降, 如果<0,则上升.
+        for i in range(left,right): #left<right
+            if (self.info[i]-value)*init<0 : # 异号
+                return i
+    def __get_tau(self,x,a,b,c):
+        return a-b*np.exp(-c*x)
     def get_paras_for_mountain(self, p1, p2):
         # amp = self.Amp(p1,p2)
         time_s = time[p1]
@@ -74,18 +81,8 @@ class Hander_index():
         amp = top_value - self.get_baseline(top_index)
         auc = sum(self.info[p1:p2])
         half_top = self.get_baseline(top_index) + amp / 2
-        half_right = top_index
-        for i in range(top_index,p2):
-            if self.info[i]<half_top:
-                half_right = i
-                break
-        half_left = top_index
-        for i in range(top_index,p1,-1):
-            if  self.info[i]<half_top:
-                half_left = i
-                break
-        # half_right = self.info[top_index:p2].index(half_top)
-        # half_left = self.info[p1:top_index].index(half_top)
+        half_left = self.__get_near_index(half_top,p1,top_index)
+        half_right = self.__get_near_index(half_top,top_index,p2)
         t_half = time[half_right] - time[half_left]
 
         right_blow_value = min(self.info[top_index:p2])
@@ -93,22 +90,39 @@ class Hander_index():
         right_amp = top_value - right_blow_value
         right_amp_90 = right_blow_value + right_amp * 0.9
         right_amp_10 = right_blow_value + right_amp * 0.1
-        right_amp_90_index = self.info[top_index:right_blow_index].index(right_amp_90)
-        right_amp_10_index = self.info[top_index:right_blow_index].index(right_amp_10)
+        right_amp_90_index = self.__get_near_index(right_amp_90,top_index,right_blow_index)
+        right_amp_10_index = self.__get_near_index(right_amp_10,top_index,right_blow_index)
+        # right_amp_90_index = self.info[top_index:right_blow_index].index(right_amp_90)
+        # right_amp_10_index = self.info[top_index:right_blow_index].index(right_amp_10)
         slope_90_10 = (right_amp_90 - right_amp_10) / (time[right_amp_90_index] - time[right_amp_10_index])
 
+# https://blog.csdn.net/cxu123321/article/details/101000604
+        x = np.array(range(right_amp_90_index,right_amp_10_index,1))
+        y = np.array([self.info[i] for i in x])
+        print(len(x))
+        from scipy.optimize import curve_fit
+        def fit(func,x,y):
+            (a,b,c),_ = curve_fit(func, x, y)
+            return a,b,c
+        a,b,c = fit(self.__get_tau,x,y)
+
         print(
-            "for [{}:{}]/[{}:{}], the top-value is {} with index {}, the Amplitude is [], slope is {}, T_half is {}, AUC is {}".format(
-                p1, p1, time_s, time_e, top_value, top_index, amp, slope_90_10, t_half, auc))
+            "for [{}:{}]/[{}:{}], the top-value is {} with index {}, the Amplitude is {}, slope is {}, T_half is {}, AUC is {}, a = {}. b={}.c={}".format(
+                p1, p1, time_s, time_e, top_value, top_index, amp, slope_90_10, t_half, auc,a,b,c))
+
+
 
         plt.figure(figsize=(float(80 * (p2 - p1) / len(self.info)), 3), dpi=300)
         x = range(p1, p2)
         plt.plot(x, self.info[p1:p2])
         plt.plot(x, [self.get_baseline(i) for i in range(len(x))])
-        plt.vline(top_index, self.get_baseline(top_index), top_value, color = "red")  # 竖线
+        plt.vlines(top_index, self.get_baseline(top_index), top_value,color='red')  # 竖线
+        plt.hlines(half_top,half_left,half_right,color='green')
         plt.xlabel(u"Index(S)")
         plt.title(u'{}_[{}:{}]'.format(self.info_name, p1, p2))
         plt.savefig(u'{}_[{}:{}]'.format(self.info_name, p1, p2))
+
+
 
 
     def Amp(self, p1, p2):
