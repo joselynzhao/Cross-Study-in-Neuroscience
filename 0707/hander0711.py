f8d354a1572291686d78254cabeb4d1908650fb7
joselyn <1326549593@qq.com>
2020-07-14 10:21:06 +0800

    0714

--- 0707/hander0711.py
+++ 0707/hander0711.py
@@ -32,7 +32,7 @@ data_array = np.array(data_array)
 time = list(data_array[:, 0])
 cab = list(data_array[:, 1])
 ca = list(data_array[:, 2])
-
+outfile = open('resoult.txt','a')
 
 # print('llwleghwoe ')
 
@@ -67,11 +67,23 @@ class Hander_index():
 
     def __get_near_index(self,value,left,right):
         init  = self.info[left]-value # 若果>0, 则下降, 如果<0,则上升.
-        for i in range(left,right): #left<right
+        for i in range(left,right,1): #left<right
             if (self.info[i]-value)*init<0 : # 异号
-                return i
+                return int(i)
+
+
+    def __get_near_index1(self,value,left,right):
+        min = 100
+        min_index = left
+        for i in range(left,right,1):
+            if abs(self.info[i]-value)<min:
+                min  = abs(self.info[i]-value)
+                min_index = i
+        return min_index
+
     def __get_tau(self,x,a,b,c):
         return a-b*np.exp(-c*x)
+
     def get_paras_for_mountain(self, p1, p2):
         # amp = self.Amp(p1,p2)
         time_s = time[p1]
@@ -81,8 +93,8 @@ class Hander_index():
         amp = top_value - self.get_baseline(top_index)
         auc = sum(self.info[p1:p2])
         half_top = self.get_baseline(top_index) + amp / 2
-        half_left = self.__get_near_index(half_top,p1,top_index)
-        half_right = self.__get_near_index(half_top,top_index,p2)
+        half_left = self.__get_near_index1(half_top,p1,top_index)
+        half_right = self.__get_near_index1(half_top,top_index,p2)
         t_half = time[half_right] - time[half_left]
 
         right_blow_value = min(self.info[top_index:p2])
@@ -90,14 +102,14 @@ class Hander_index():
         right_amp = top_value - right_blow_value
         right_amp_90 = right_blow_value + right_amp * 0.9
         right_amp_10 = right_blow_value + right_amp * 0.1
-        right_amp_90_index = self.__get_near_index(right_amp_90,top_index,right_blow_index)
-        right_amp_10_index = self.__get_near_index(right_amp_10,top_index,right_blow_index)
+        right_amp_90_index = self.__get_near_index1(right_amp_90,top_index,right_blow_index)
+        right_amp_10_index = self.__get_near_index1(right_amp_10,top_index,right_blow_index)
         # right_amp_90_index = self.info[top_index:right_blow_index].index(right_amp_90)
         # right_amp_10_index = self.info[top_index:right_blow_index].index(right_amp_10)
         slope_90_10 = (right_amp_90 - right_amp_10) / (time[right_amp_90_index] - time[right_amp_10_index])
 
 # https://blog.csdn.net/cxu123321/article/details/101000604
-        x = np.array(range(right_amp_90_index,right_amp_10_index,1))
+        x = np.array(range(right_amp_90_index-top_index,right_amp_10_index-top_index,10))
         y = np.array([self.info[i] for i in x])
         print(len(x))
         from scipy.optimize import curve_fit
@@ -105,17 +117,17 @@ class Hander_index():
             (a,b,c),_ = curve_fit(func, x, y)
             return a,b,c
         a,b,c = fit(self.__get_tau,x,y)
-
-        print(
-            "for [{}:{}]/[{}:{}], the top-value is {} with index {}, the Amplitude is {}, slope is {}, T_half is {}, AUC is {}, a = {}. b={}.c={}".format(
-                p1, p1, time_s, time_e, top_value, top_index, amp, slope_90_10, t_half, auc,a,b,c))
-
-
+        outfile.write(' '.join(map(str,[p1,p2,time_s,time_e,top_value,top_index,amp,slope_90_10,t_half,auc,a,b,c]))+'\n')
+        # print(
+        #     "for [{}:{}]/[{}:{}], the top-value is {} with index {}, the Amplitude is {}, slope is {}, T_half is {}, AUC is {}, a = {}. b={}.c={}".format(
+        #         p1, p1, time_s, time_e, top_value, top_index, amp, slope_90_10, t_half, auc,a,b,c))
 
         plt.figure(figsize=(float(80 * (p2 - p1) / len(self.info)), 3), dpi=300)
         x = range(p1, p2)
         plt.plot(x, self.info[p1:p2])
-        plt.plot(x, [self.get_baseline(i) for i in range(len(x))])
+        plt.plot(x, [self.get_baseline(i) for i in range(p1,p2)])
+        x2 = range(right_amp_90_index,right_amp_10_index,1)
+        plt.plot(x2,[self.__get_tau(i-top_index,a,b,c) for i in x2])
         plt.vlines(top_index, self.get_baseline(top_index), top_value,color='red')  # 竖线
         plt.hlines(half_top,half_left,half_right,color='green')
         plt.xlabel(u"Index(S)")
@@ -124,25 +136,15 @@ class Hander_index():
 
 
 
-
-    def Amp(self, p1, p2):
-        top = max(self.info[p1:p2])
-        index = self.info[p1:p2].index(top)
-        return top - self.get_bwaseline(index)
-
-    def draw_mountain(self, p1, p2):
-        plt.figure(figsize=(float(80 * (p2 - p1) / len(self.info)), 3), dpi=300)
-        x = range(p1, p2)
-        plt.plot(x, self.info[p1:p2])
-        plt.plot(x, [self.get_baseline(i) for i in range(len(x))])
-        # plt.vline(, -2, 3, color="red")  # 竖线
-        plt.xlabel(u"Index(S)")
-        plt.title(u'{}_[{}:{}]'.format(self.info_name, p1, p2))
-        plt.savefig(u'{}_[{}:{}]'.format(self.info_name, p1, p2))
-
-
+outfile = open('resoult.txt','a')
+outfile.write('p1 p2 time1 time2 top_value top_index amp,slope t_half auc a b c\n')
 hander = Hander_index(ca, 'ca', 4000)
 # print(hander.Amp(6800,7200))
-hander.get_paras_for_mountain(60100, 62500)
+segfile = open('res.txt','r')
+seg_data = segfile.readlines()
+for line in seg_data:
+    left,right = map(int,line.strip().split())
+    print('[{}:{}]'.format(left,right))
+    hander.get_paras_for_mountain(left, right)
 # hander.full_signal()0
 # hander.full_signal(ca,'ca')
